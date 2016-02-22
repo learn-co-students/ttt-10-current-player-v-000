19e5afd4e8a4fa501b7536085febc621940371fc
PeteAJ <peter.jacobson@gmail.com>
2016-02-22 16:14:12 -0500

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+
+  counter = 0
+if my_condition_is_met
+  counter += 1
+end
+
+
+board = ["X", "O", " "]
+board.each do |times|
+  puts "#{times} turn(s) has been played!"
+end
+  end
+
+
+
+def current_player(board)
+  end
