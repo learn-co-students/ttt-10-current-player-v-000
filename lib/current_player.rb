69d2dfb1ecd7b689107e2d54eb55133714a54547
RMcNeely <rmcneely23@gmail.com>
2016-02-22 15:46:46 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,1 +1,10 @@
-turn.each do {counter +=}
+def turn_count(board)
+  board.count("X") + board.count("O")
+end
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
+end
