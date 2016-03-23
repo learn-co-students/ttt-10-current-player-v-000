e7f4c30ea12b7e47fe87a9f3d3011b34aae66fc6
kellyayers411 <kellyayers411@gmail.com>
2016-03-23 01:32:16 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,16 +1,18 @@
 def turn_count(board)
   counter = 0
-  board.each|spot| do 
-    if spot == "X" || spot == "O"
+  board.each do |spot|
+    if (spot == "X" || spot == "O")
       counter += 1
     end
-  if counter % 2 = 0
-    player = "O"
-    else
-    player = "X"
   end
+  return counter
 end
- 
-def current_player
-  
+
+def current_player(board)
+ counter = turn_count(board)
+ if counter % 2 == 0
+    return "X"
+    else
+    return "O"
+  end
 end
