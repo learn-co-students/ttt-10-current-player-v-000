697efd15fdd0d0e71d52c151c5d2cd4b449dd41a
spetluri <spetluri.1@gmail.com>
2016-03-27 23:47:45 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,20 @@
-
 def turn_count(board)
   count = 0
   board.each do |pos|
-    if (pos == "X") || (pos == "Y")
+    if (pos == "X") || (pos == "O")
       count+=1
     end
   end
   return count
 end
 
+def current_player(board)
+  count = turn_count(board)
+  if count % 2 = 0
+    "X"
+  else
+    "O"
+
+end
+  
 
