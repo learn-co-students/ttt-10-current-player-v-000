5a98c21606941c6c02b94b41953c8f83f759ac88
miggyteez <mjtavarez@yahoo.com>
2016-03-28 22:13:55 -0400

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,25 @@
+def turn_count(board)
+  turns_past = 0
+
+  board.each do |cell|
+    if cell == "X" || cell == "O"
+      turns_past += 1
+    end
+  end
+  turns_past
+end
+
+def current_player(board)
+  # if turn_count(board).even?
+  #   return "X"
+  # else
+  #   return "O"
+  # end
+
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+
+end
