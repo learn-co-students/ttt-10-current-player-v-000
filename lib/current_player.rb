b4f42cf51f74b1f9298818935631af2b9b5876cc
Scott Wakeman <swakeman@ca.rr.com>
2016-06-28 02:34:13 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,28 @@
+def turn_count(board)
+  counter = 0
+  board.each{|token|}
+  token == "X" || "O"
+  if turn_count == 4 % 2
+    current_player = "X"
+  else
+    current_player = "O"
+  counter +=1
+      end
+    end
+
+#def current_player["X","O"]
+#end
+
+#def current_player["X","O"]
+    #current_player.each{ |player|  }
+    #current_player = "X"
+        #else
+          #current_player = "O"
+  #end
+      #end
+
+#def current_player["X", "O"]
+#current_player.each do |player|
+      #end
+
+#current_player(board)
