3b312351de5920f98ce2781be45ad3b52ae22f0e
alispace <alispace410@gmail.com>
2016-07-25 07:16:59 -0400

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,19 @@
 def turn_count(board)
-  
-  turns = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      turns += 1
+  count = 0
+  board.each do |ino|
+    if ino == "X" || ino == "O"
+      count +=1
     end
-  end
-  turns
 
+  end
+  count
 end
 
+
 def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : "O"
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
 end
