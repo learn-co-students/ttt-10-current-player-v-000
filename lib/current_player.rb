d2bdfeafa9da2ad860f2124715622f60f038ec24
charlie lee <ctlee9096@gmail.com>
2016-08-25 12:07:57 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,21 +1,3 @@
-def turn(board)
-  puts "Please enter 1-9:"
-  input= gets.chomp
-  input= input_to_index(input)
-  if valid_move?(board,input)
-    move(board,input)
-    display_board (board)
-  #move(board, input)  display(board)#here was the mistake, for the method called is not correctly ID
-  #input>=0 || input<=8 move(board,input) this was already validated in valid_move method
-  elsif
-    turn(board) #here is the missing line for 9-12 pm (3 hrs) last nt and 9-11 am (2 hrs today), method calls itself is a new concept
-  end
-end
-  #input=input_to_index(input)
-  #while !input.between?(0,8)  puts "Please enter 1-9:"   end
-  #while input.between?(0,8)  move(board, input) display(board) end
-  #while !input.between?(0,8) puts "Please enter 1-9:"  end
-
 def turn_count (board)
   #if only two occupied position, then player "o" made move
   #if only one occupied position, then player "x" made move
@@ -23,14 +5,25 @@ def turn_count (board)
   #if board.each do |item| == "X" || board.each do |item| == "O"
   #if board.each do |item| =="X" || board.each do |item| =="O"
   #if board.each do |item| != ""
+  #if board.each do |item| == "X", got sytax error, unexpected ==^ "X"
   board.each do |item|
-    item != ""
+     if item != " "
+    #if item !==" "
+    #if item !== " "
+    #if item == "X" ; worked
+    #if item == "X" # got 2, expected 3 error message
+    #if item = "O" || item = "X"
+    #if item != "O" || item != "X"
+    #while item != ""
     #item = "X"
-    #if item == "X" || item == "O"
+    #if item == "X" || item == "O" got: ["0", " ", ****]
     #if item == "X"
     #if item != ""
     #if item !=""
     counter +=1
+  #elsif item == "O"; worked
+    #counter +=1; worked w/ line 23
+  end
     end
   #desired outcome is number of turns that have been made by
   counter
@@ -38,3 +31,16 @@ end
   #desired outcome is number of turns that have been made by
     #setting some counter, starting @ 0, to iterate over each member of
     #board array, to see if element is "x" or "o" by 1 increments
+def current_player (board)
+  #def current_player
+  #def current_player (board) # wrong number of arguments (0 for 1)
+  #def current_player () # wrong number of arguments (1 for 0)
+  #def current_player (board, first = "X", second = "O")
+  if turn_count % 2 == 0
+    return "O"
+    #puts "O", returns nil. same as print command
+  elsif turn_count % 2 == 1
+    return "X"
+    #puts "X", return nil. same as print command
+  end
+end
