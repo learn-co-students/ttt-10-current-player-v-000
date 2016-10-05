2ab2bcc7ce67401ab3685598ca0a86e14c9bf861
Sarah Rosenshine <srosenshine@gmail.com>
2016-10-05 19:55:19 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,25 +1,11 @@
-# def current_player(board)
-#   turn_count(board) % 2 == 0 ? "X" : "O"
-# end
-
-def turn_count(board)
-  board.count {|entry| entry == "X" || entry == "O"}
-end
-
-# def turn_count(board)
-#   counter = 0
-#   board.each do |e|
-#     if e != "" || e != " "
-#       counter += 1
-#     end
-#   end
-#   learn test
-# end
-#
 def current_player(board)
   if turn_count(board).even?
     "X"
   else
     "O"
   end
+end
+
+def turn_count(board)
+  board.count {|entry| entry == "X" || entry == "O"}
 end
