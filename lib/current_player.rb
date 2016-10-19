bd7fcc2346dc0953f5080b37eeb8354b22cfe025
Kym  Rohman <kymrohman@gmail.com>
2016-10-19 23:51:14 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  counter = 0
+  board.each do |play|
+    if  play == "X" || play == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
+
+def current_player(board)
+  play = turn_count(board) % 2
+  if play == 0
+    return "X"
+  else
+    return "O"
+  end
+end
+#Then, try refactoring your method to use the .even? or .odd? methods. Look them up in the Ruby Docs to learn more.
