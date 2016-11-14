902ff406a34567017b09feb7ac3d50fa273fef11
Daniel Goldberg <danpaulgo@aol.com>
2016-11-14 17:31:56 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  count = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  moves = turn_count(board)
+  if moves.even?
+    "X"
+  else
+    "O"
+  end
+end
