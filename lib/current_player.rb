488d5db70d2ae3bdf7dc1d76bda31c797e4552dc
Shannon McCormack <shannonkmc@hotmail.com>
2016-12-06 19:56:28 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+
+def turn_count(board)
+  counter = 0
+  board.each do |index|
+   if index=="X" || index=="O"
+   counter += 1
+   end
+  end
+  counter
+end
+
+def current_player(board)
+  turn_count(board)
+  if turn_count(board).even? 
+  "X"
+  else
+  "O"
+end
+end
