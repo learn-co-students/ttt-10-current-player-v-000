51c501968cfa26f4d56d6e4d989ebdc50d032f2b
Elias Ventura <eliasventura23@gmail.com>
2016-12-10 04:06:00 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,13 @@
 def turn_count(board)
-
   counter = 0
-
-  board.each do |token|
-    if token == "X" || token == "O"
-  counter += 1
-end
-end
-counter
+  board.each do |element|
+    if element == "X" || element == "O"
+      counter += 1
+    end
+  end
+  counter
 end
 
 def current_player(board)
-
-  if turn_count(board) % 2 == 0
-    "X"
-  else turn_count(board) % 2 == 1
-    "O"
-  end
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
