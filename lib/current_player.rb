ed6f34273f90d34e424d4b9bf3570361660ebaf2
Aaron R. Trevino <aaronrtrevino12@gmail.com>
2017-01-06 13:04:53 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,4 @@
-board = ["X", " ", "O", " ", "X", " ", " ", " ", "O"]
+board = ["X", "O", " ", " ", "X", " ", " ", " ", "O"]
 
 def turn_count(board)
  counter = 0
@@ -11,11 +11,13 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board).even?
+#  if turn_count(board).even?
 # above code works like this --> if turn_count(board) % 2 == 0
-    return "X"
-  else return "O"
-  end
+#    return "X"
+#  else return "O"
+#  end
+  turn_count(board).even? ? "X" : "O"
+# above is ternary operator that replaces if/else
 end
 
 puts current_player(board)
