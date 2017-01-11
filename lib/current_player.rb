ef7e76cd8370816b0d6fa9da183d371cb829e66b
Damien de Lartigue <damien@delartigue.co.uk>
2017-01-11 02:52:00 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,20 @@
 def turn_count(board)
   turns = 0
   board.each do |spaces|
+    puts turns
     if spaces == "X" || spaces == "O"
     turns += 1
+    end
   end
   turns
 end
 
-def current_player
-  turn_count % 2 == 0 ? "O" : "X"
+def current_player(board)
+  turn = turn_count(board)
+
+  if turn % 2 == 0 || turn == 0
+    return "X"
+  else
+    return "O"
+  end
 end
