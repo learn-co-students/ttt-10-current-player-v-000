a3d9bf17577a68ceab1a04f7ddc8979fb70d9ebd
Dalma Boros <dalma.boros@gmail.com>
2017-01-26 16:06:52 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,8 +6,16 @@ def turn_count_original_method(board)
   counter
 end
 
-def turn_count(board)
+def turn_count_second_method(board)
   board.count { |position| position == "X" || position == "O"}
+end
+
+def turn_count(board)
+  board.count { |marker| x_or_o?(marker) }
+end
+
+def x_or_o?(marker)
+  marker == "X" || marker == "O" && marker != " " && marker != "" && marker != nil
 end
 
 def current_player(board)
