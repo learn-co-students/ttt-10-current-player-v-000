e0e69395eacdccd1c6bd6cf2aa2f1cd68b869044
Danielle Croxton <danikrist@gmail.com>
2017-02-20 20:01:13 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+
+
+def turn_count(board)
+  turn = 0
+  board.each do |board_space|
+    if board_space != " "
+      turn += 1
+  end
+  end
+      return turn
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
