af9e8fb2ece009942f0d853c2fd4d7ab03ae711c
Zain Halai <zain@halai.us>
2017-03-04 16:19:41 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,11 @@
+def turn_count(board)
+  count = 0
+
+  board.each { |char| count += 1 if char == "X" || char == "O" }
+
+  count
+end
+
+def current_player(board)
+  ( turn_count(board).even? ) ? "X" : "O"
+end
