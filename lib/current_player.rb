259ccc786c15b1665cbf15f5d0689cc489f344b2
Andrew Bonner <andrewbonner92@gmail.com>
2017-03-12 17:58:20 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+def turn_count(board)
+  counter = 0
+
+  board.each do |move|
+    if move != " "
+      counter += 1
+    end
+  end
+
+  return counter
+end
+
+def current_player(board)
+  current_turn = turn_count(board)
+
+  if current_turn % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
