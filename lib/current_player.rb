53aa34ee9ca5e5e752ab2728a380a3eba6c3c286
Deborah Johnson <debjohnson_11@yahoo.com>
2017-04-03 16:15:35 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+    turn = 0
+    board.each do | position |
+      if position == 'X' || position == 'O'
+        turn += 1
+
+      end
+    end
+    return turn
+end
+
+def current_player(board)
+    if turn_count(board).even?
+        return "X"
+    else
+        return "O"
+    end
+end
