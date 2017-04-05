a90b65bfe2ab784bd9a2eff6e1c1a2ec46bdd0d2
Karinna Iniguez <karinnainiguez@gmail.com>
2017-04-05 03:41:39 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,21 @@
+
+
+def turn_count(board)
+  turn=0
+  board.each do |index|
+    if index=="X"|| index=="O"
+      turn+=1
+    end
+  end
+  turn
+end
+
+
+def current_player(board)
+  turn=turn_count(board)
+  if turn%2==0
+    "X"
+  else
+    "O"
+  end
+end
