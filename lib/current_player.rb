723e4db0814aab15d277d83d0785462849abd35e
Rosie Martinez <rosiemin11@gmail.com>
2017-04-06 15:45:30 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  count = 0
+  board.each do |space|
+    if space == "O"
+      count += 1
+    elsif space == "X"
+      count += 1
+    end
+  end
+  return count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
