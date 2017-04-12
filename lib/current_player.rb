508ae527e00f9f81c9d2a2b9e5e9928a2a959f22
Erin Land <erinland05@gmail.com>
2017-04-12 03:01:18 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,19 @@
 def turn_count(board)
-  counter = 0
-board.each do |space|
-  if board[] == "X" || "O"
-    counter += 1
+  counter = 0 
+  board.each do |space|
+  if space == "X" 
+      counter += 1
+    elsif space == "O" 
+        counter += 1
+    end
+  end
+return counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    return "X"
+  else
+    return "O" 
   end
 end
