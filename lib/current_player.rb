ef7a37b12c0bd27d78500b44884cbb61b565a1a2
Nathan  Visak <nathan.visak@gmail.com>
2017-07-08 17:29:01 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,29 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  index = input_to_index(input)
+  if valid_move?(board, index)
+    move(board, index)
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+    counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
