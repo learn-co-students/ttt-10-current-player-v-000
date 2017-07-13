7fa504c48fab0f316bbebff1342614a2b3e54031
Amberly Armstrong <armstrong.amberly@gmail.com>
2017-07-13 23:09:12 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+board = ["X", "O", "X", "O", "X", "O", "X", "O", "X"]
+count = 0
+
+def turn_count(board)
+  count = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  turn_count(board).even? ? "X" : "O"
+end
