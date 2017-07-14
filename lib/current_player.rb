5c564378172f1a60f3fb71a2a447b181f6a87ffa
Camille Neuner <neuner.camille@gmail.com>
2017-07-14 05:34:57 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -14,8 +14,16 @@ def turn_count(board)
   counter
 end
 
+#def current_player(board)
+  #if turn_count(board) % 2 == 0
+  #  return "X"
+  #else
+  #  return "O"
+  #end
+#end
+
 def current_player(board)
-  if turn_count(board) % 2 == 0
+  if turn_count(board).even?
     return "X"
   else
     return "O"
