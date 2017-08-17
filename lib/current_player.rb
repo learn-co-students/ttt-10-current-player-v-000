0bd0b54250b8ef8f5013b718b83c6cc9ad414696
Ally Kadel <akadel01@gmail.com>
2017-08-17 18:23:18 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,17 @@
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else return "O"
+  end
+end
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |element|
+    if element == "X" || element == "O"
+      counter += 1
+    end
+  end
+  return counter
+end
