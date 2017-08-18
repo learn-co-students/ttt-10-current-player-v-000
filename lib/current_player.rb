7b6b781e167c3bc4615679ea68ae312f02a6c4ff
Gui Bin Zhang <guibzhang@gmail.com>
2017-08-18 03:07:45 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board) #determining which players turn is it
+  turns = 0
+  board.each do |player|
+    if player == "X" || player == "O"
+      turns += 1
+    end
+  end
+  turns
+
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
