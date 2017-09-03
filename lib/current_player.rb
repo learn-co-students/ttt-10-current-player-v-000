178e8b22b263295608fefb7134308bdd72d971c7
Tuong Van Nguyen <tvan262@yahoo.com>
2017-09-03 17:03:18 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,27 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def turn_count(board)
+  counter = 0
+  board.each do |positions|
+    if positions != " "
+      counter += 1
+    end
+  end
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+      return "X"
+  elsif turn_count(board) % 2 == 1
+      return "O"
+  end
+end
