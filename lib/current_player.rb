b3ebd33ef10166c60456f94e1407d26435019aa5
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-09-15 23:59:44 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,24 @@
+def turn_count(board)
+  counter = 0
+  board.each do |position|
+   if position == "X" || position == "O"
+     counter += 1
+  end
+ end
+ counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+# variables
+# methods
+# iterators
+# conditionals
+
+# code academy
+# google ruby docs
