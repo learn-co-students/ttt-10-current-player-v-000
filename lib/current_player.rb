b7a9d5f0278a8232976d10c988da7aa6458bafe2
Blakeley Bond <burrick2003@yahoo.com>
2017-10-03 03:15:52 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,19 +2,19 @@
 def turn_count(board)
   turns=0
   board.each do |board_space|
-    if board_space == "O"
+    if board_space == "O" || board_space == "X"
       turns +=1
     end
-    if board_space == "X"
-      turns += 1
-    end
+    #if board_space == "X"
+    #  turns += 1
+    #end
   end
   turns
 end
 
 #Converts even/odd number of board elements to whose turn it is
 def current_player(board)
-player = "X"
+#player = "X"
 if (turn_count(board) % 2) == 0
   player = "X"
 elsif (turn_count(board) % 2) != 0
