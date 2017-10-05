5abe4deb57b47825b0812bd3ea831ee32a7c812b
Joshua Needham <joshuabneedham@gmail.com>
2017-10-05 02:01:08 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,14 +1,16 @@
 def turn_count(board)
   moves = 0
+  # Checking each position on the board for an entry
   board.each do |move|
-    # Checking each position on the board for an entry
     # If not empty increment moves by one
     if move == "X" || move == "O"
       moves += 1
     end
   end
+  #return moves with or without 'return'
   moves
 end
+
 def current_player(board)
   # if turn_count(board) % 2 == 0
   #   return "X"
