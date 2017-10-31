4a320efebd72aaa944161897a67e7638e0e21940
Keith  Polischuk <kpolischuk@gmail.com>
2017-10-31 16:11:23 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,12 @@
 
 def turn_count(board)
 counter = 0
-  board.each do |space|
-    if space == "X" || space == "O"
+  board.each do |token|
+    if token == "X" || token == "O"
       counter += 1
 end
 end
-return counter
+counter
 end
 
 def current_player(board)
