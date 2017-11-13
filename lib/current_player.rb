b92f6acced48f84f605882338feef695283fc08c
James Dolan <jwdolan820@gmail.com>
2017-11-13 04:15:03 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   counter = 0
   board.each do |turn|
-    if turn == "X" || turn =="O"
+    if turn == "X" || turn == "O" #if turn = X or O, increment counter
       counter += 1
     end
   end
@@ -9,5 +9,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-    turn_count(board) % 2 == 0 ? "X" : "O"
+  turn_count(board) % 2 == 0 ? "X" : "O" #returns X if remainder = 0, O if not
 end
