9ad51f533d24a5a315a09b8ddb23eb29873caed6
Jeffrey Withers <jeffreywithers@gmail.com>
2017-11-14 21:12:27 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,9 +12,17 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
+  if turn_count(board).even?
     "X"
-  elsif turn_count(board) % 2 != 0
+  elsif turn_count(board).odd?
     "O"
   end
 end
+
+# def current_player(board)
+#   if turn_count(board) % 2 == 0
+#     "X"
+#   elsif turn_count(board) % 2 != 0
+#     "O"
+#   end
+# end
