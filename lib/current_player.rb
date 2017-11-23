4626814462049e7037a2809047ef500cd8e53c3c
Kriti Rai <kriti.rai@gmail.com>
2017-11-23 06:31:21 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    current_player = "X"
-  else
-    current_player = "O"
-  end
+  turn_count(board) % 2 == 0? "X" : "O"
+  #   current_player = "X"
+  # else
+  #   current_player = "O"
+  # end
 end
