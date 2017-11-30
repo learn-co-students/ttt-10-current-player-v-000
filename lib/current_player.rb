8ccd7d4c317450833d3aa797f0bbf5f8a554d2f1
Aubrey Neimeier <neimeier.1@osu.edu>
2017-11-30 18:38:07 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,4 +1,5 @@
 def turn_count(board)
+
   num_turns = 0
   board.each do |turns|
       if turns == "X" || turns =="O"
@@ -6,6 +7,7 @@ def turn_count(board)
         end
     end
     num_turns
+
 end
 
 def current_player(board)
@@ -17,9 +19,16 @@ def current_player(board)
   end
 end
 
+#formal solution for current_player
+# def current_player(board)
+#  if turn_count(board) % 2 == 0 ? "X" : "O"
+#end
+
+#the above doesn't even declare a variable, it just compaares the retun of turn_
+#count(board) directly to remainder.
 
 #remember variables have limited scope. We have to declare num(turns) IN our
 #current_player method and assign it to the return value of turn_count.
 
 #board is an argurment becuase it is required to execute turn_count, which is
-#called in this method. 
+#called in this method.
