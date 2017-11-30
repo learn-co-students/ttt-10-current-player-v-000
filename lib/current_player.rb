8e0a41da69763bb5a45ecf831320906502455561
Aubrey Neimeier <neimeier.1@osu.edu>
2017-11-30 18:33:43 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,12 +8,18 @@ def turn_count(board)
     num_turns
 end
 
-#def current_player(num_turns)
-#  if turn_count % 2 == 0
-#    "X"
-#  else
-#    "O"
-#  end
-#end
+def current_player(board)
+  num_turns = turn_count(board)
+ if num_turns % 2 == 0
+   "X"
+  else
+    "O"
+  end
+end
 
-#num_turns = turn_count(board)
+
+#remember variables have limited scope. We have to declare num(turns) IN our
+#current_player method and assign it to the return value of turn_count.
+
+#board is an argurment becuase it is required to execute turn_count, which is
+#called in this method. 
