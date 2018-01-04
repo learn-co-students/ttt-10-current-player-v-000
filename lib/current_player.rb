bc90485279cde88964a8b221cb93fb86756e0241
Hanna Vanderloop <hvanderloop@gmail.com>
2018-01-04 23:54:35 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,7 @@
-
 def turn_count(board)
-counter = 0
-  board.each do |token|
-    if token != " "
+  counter = 0
+  board.each do |character|
+    if character != " "
       counter += 1
     end
   end
@@ -10,9 +9,9 @@ counter = 0
 end
 
 def current_player(board)
-if turn_count(board).even?
-  "X"
-else turn_count(board).odd?
- "O"
-end
+  if turn_count(board).even?
+    "X"
+  else turn_count(board).odd?
+    "O"
+  end
 end
