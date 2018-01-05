5d683f5ee0ae217604bd7e07157c5768dcdc2520
Kim Gonzales <kimgo589@gmail.com>
2018-01-05 01:22:49 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -4,10 +4,19 @@ def turn_count(board)
   end
 end
 
+=begin
 def current_player (board)
   if turn_count(board) % 2 == 0
     "X"
   else
     "O"
   end
+end
+=end
+
+
+#Using the ternary operator:
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
