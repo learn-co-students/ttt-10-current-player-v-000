6dc482ed90e984dfb2d2dc75c56eb353130beb93
Brandon Brasson <rocksong622@gmail.com>
2018-01-27 19:32:21 +0000

    Done.

--- spec/current_player_spec.rb
+++ spec/current_player_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/current_player.rb'
 
-describe "./lib/current_player.rb" do
+puts describe "./lib/current_player.rb" do
   describe '#turn_count' do
     it 'counts occupied positions' do
       board = ["O", " ", " ", " ", "X", " ", " ", " ", "X"]
@@ -9,23 +9,23 @@ describe "./lib/current_player.rb" do
     end
   end
 
-  describe '#current_player' do
+  puts describe '#current_player' do
     it 'returns the correct player, X, for the first move' do
       board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
       expect(current_player(board)).to eq("X")
     end
 
-    it 'returns the correct player, O, for the second move' do
+    puts it 'returns the correct player, O, for the second move' do
       board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 
       expect(current_player(board)).to eq("O")
     end
 
-    it 'returns the correct player, X, for the third move' do
+    puts it 'returns the correct player, X, for the third move' do
       board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
 
       expect(current_player(board)).to eq("X")
     end
-  end  
+  end
 end
