2b86226b24aaf2578e9ae30bfdae8ee2461d654e
Eva Smidt <shevangeline@gmail.com>
2018-02-01 02:01:20 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,34 +1,19 @@
-#def turn_count(board)
-#def turn_count(board)
-#turns_counter = 0
-#board.each do |turn|
-#if board == "X" || board ==  "O"
-  #turns_counter += 1
-  #turns_counter
-#end
-#turns_counter
-#end
-#end
-
-
 def turn_count(board)
-turns_counter = 0
-board.each do |turn|
-if turn == "X" || turn == "O"
-turns_counter += 1
+turns = 0
+board.each do |token|
+if token == "X" || token == "O"
+turns += 1
 end
 end
-turns_counter
+turns
 end
 
 
 
 def current_player(board)
-
 if turn_count(board) % 2 == 0
   return "X"
 else
-#turn_count(board) !% 2
   return "O"
 end
 end
