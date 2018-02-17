28c15542de03d266ae95a34d3c03ea1f0e170d00
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-02-17 13:55:27 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,9 +12,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
+  if turn_count(board).even?
     "X"
-  else
+  elsif turn_count(board).odd?
     "O"
   end
 end
