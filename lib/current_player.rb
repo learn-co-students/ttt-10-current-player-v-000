a3e8fc00ae02f7be5fbc8661408bbfd4cac8e46e
Angela Campion <campion.angela@gmail.com>
2018-04-19 21:17:52 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+require 'pry'
+def turn_count(board)
+  count = 0
+  board.each do |move|
+    if move == "X" || move == "O"
+      count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  player = " "
+  if turn_count(board) % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
+end
