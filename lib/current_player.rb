1392f9fd59295094795dde7cc5a7e384394ca7e6
Dale Terrell <tragicid@gmail.com>
2018-04-25 16:55:45 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,7 @@
 def turn_count(board)
   counter = 0
-  #if current_player
-   # counter +=1
-#end
   board.each do |v|
-    if v=='X'||v=='O'
+    if v == 'X'||v == 'O'
       counter +=1
     end
   end 
@@ -12,7 +9,7 @@ def turn_count(board)
 end
 
 def current_player
-  if turn_count % 2==0
+  if turn_count % 2 == 0
     current_player = 'X'
   else
     current_player = 'O'
