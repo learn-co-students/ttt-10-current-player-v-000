0cccf7472398b24f2e9e768647f8315574ed3d89
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-20 03:14:16 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -12,6 +12,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  turn_count(board) % 2 == 0 ? "X" : 0
+  if turn_count(board) % 2 == 0 
+    return "X"
+  else
+    return "O"
   end
 end
