91d5bc900731288b8e5b8701b6bc8ad2138e4f3b
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-20 18:51:08 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,17 @@
-require 'pry'
-
 def turn_count(board)
   counter = 0 
-  board.each do |token| 
-    if token == "X" || token == "O"
+  board.each do |coin|
+    if coin == "X" || coin == "O"
       counter += 1 
-      binding.pry
     end
   end
   counter
 end
 
 def current_player(board)
-  turn_count(board).even? ? % "X" : "O"
-  turn_count
+  if turn_count(board) % 2 == 0 
+    return "X"
+  else
+    return "O"
+  end
 end
