764dbd7fe40b9208fd00fc7e67de3c60ff6f7b20
Joseph Senders <jasenders@gmail.com>
2018-05-27 15:17:32 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,33 @@
+def turn_count(turn)
+  #counter = 1
+  move_count = []
+  turn.each do |unit|
+    #counter = 0
+    if unit == "X" || unit == "O"
+      #puts "spot taken"
+      move_count.push(1)
+    #else
+      #puts "spot free"
+      #counter += 1
+    end
+  end
+  move_count.length.to_i
+end
+
+def current_player(board)
+  #if turn_count(board) %  == 0
+    num = turn_count(board)
+    if num % 2 == 0
+      return "X"
+    else
+      return "O"
+    #puts "divisible by 2"
+    end
+end
+
+board = ["X","X"," "," ",""," "," "," "," "]
+#puts "Welcome to Tic Tac Toe!"
+#puts display_board(board)
+#play(board)
+#turn_count(board)
+current_player(board)
