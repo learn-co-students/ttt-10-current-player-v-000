7380c5c820e087a283b08dc104e30905b8be7857
Morgan Edmonds <edmonds.morgan@gmail.com>
2018-05-30 01:21:42 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,18 @@
+def turn_count(board)
+  turn_count = 0
+  board.each do |element|
+    if element == "X" or element == "O"
+      turn_count += 1
+    end
+  end
+  turn_count
+end
+  
+def current_player(board)
+  if turn_count(board).even?
+    "X"
+  else
+    "O"
+  end
+end
+
