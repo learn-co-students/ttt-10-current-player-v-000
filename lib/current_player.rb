ed01cf1bc18a05b8f686cbfc391a3f37e2c5208e
Hayden Betts <haydenbetts@gmail.com>
2018-06-21 20:31:34 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+def turn_count(board)
+  turn_counter = 0
+
+  board.each do |position|
+    if position == "X" || position == "O"
+      turn_counter += 1
+    end
+  end
+
+  return turn_counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
