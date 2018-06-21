b144a4813a1a08a49680b9c64a5485037af56893
Hayden Betts <haydenbetts@gmail.com>
2018-06-21 20:38:00 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,9 @@
 def turn_count(board)
-  turn_counter = 0
+  turn_counter = 0 # number of turns played
 
   board.each do |position|
+    # nice way to get around non DRY conditions like this..
+    # turn_counter += 1 if ["X", "O"].include?(position)
     if position == "X" || position == "O"
       turn_counter += 1
     end
@@ -11,9 +13,5 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    "X"
-  else
-    "O"
-  end
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
