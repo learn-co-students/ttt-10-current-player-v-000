7e07a959d311ed6e856407727c9de178678b1aeb
Amee Covarrubias <ameecova@yahoo.com>
2018-07-13 06:44:06 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(board)
+  counter = 0 
+  board.each { |turn| counter +=1 if turn != " " }
+  counter
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0 
+    return "X"
+  else 
+    return "O"
+  end
+end
