5144900013a4f1d709d1c1a60052ba2a340ae42e
Heshie Brody <4hbrody@gmail.com>
2018-07-19 18:20:38 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,19 +1,18 @@
-
-def turn_count(baord)
+def turn_count(board)
 counter = 0
-  player = board.each |player| do
-      if player == "X" || player == "O"
-        counter += 1
-      end
+board.each do |place|
+  if place == "X" || place == "O"
+    counter += 1
   end
-  current_player(player)
+end
   return counter
 end
 
-def current_player(player)
-  if player == player % 2
-    return "O"
-  elsif player == player % 1
-    return "X"
-  end
+def current_player(board)
+player = turn_count(board) % 2 == 0 ? "X" : "O"
+  # if player % 2 == 0
+  #   return "X"
+  # else
+  #   return "O"
+  # end
 end
