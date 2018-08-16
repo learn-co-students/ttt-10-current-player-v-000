3243b5206124929fa5c4594f4bdd52c6ff63b069
Allison Howard <alliemhow@gmail.com>
2018-08-16 16:41:51 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,13 @@
 def turn_count(board)
   counter = 0
-  
+
   board.each do |turn|
     if turn == "X" || turn == "O"
       counter += 1
       puts "#{counter}"
     end
   end
-    
+
   counter
 end
 
