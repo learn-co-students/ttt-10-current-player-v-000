6ff9acaaf9acfdae32c2ec892c8e477d7b6d0730
Allison Howard <alliemhow@gmail.com>
2018-08-16 16:52:51 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,20 +1,15 @@
 def turn_count(board)
-  counter = 0
-
-  board.each do |turn|
-    if turn == "X" || turn == "O"
-      counter += 1
-      puts "#{counter}"
+  # board.count{|token| token == "X" || token == "O"}
+  turns = 0
+  board.each do |token|
+    if token == "X" || token == "0"
+      turns +=1
     end
   end
-
-  counter
+  turns
 end
 
-def current_player(board)
-  if turn_count(board).even?
-    return "X"
-  else
-    return "O"
-  end
+ def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
 end
