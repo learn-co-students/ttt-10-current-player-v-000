14b0a00480dbaf1d869e5d9de5475a0b1ceab30a
Ashley Wong <ashwong04@gmail.com>
2018-08-17 16:14:39 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,17 @@
 def turn_count(board)
   counter = 0
   board.each do |count|
-    if count != "O" || count == "X"
+    if count == "O" || count == "X"
     counter += 1
     end
   end
+  return counter
+end
+
+  
+def current_player(board)
+  
+  
+  
+  
 end
