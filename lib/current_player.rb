9a183adf4916c5b5716d9bbff43b4037be8815c1
dwieb <dwayne.harmon@gmail.com>
2018-08-26 17:22:25 -0400

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,16 @@
 
 
-def turn_count(arg)
+def turn_count(board)
   counter = 0
-  arg.each do |n|
-    if n == "X" || n == "O"
+  board.each do |cell|
+    if cell == "X" || cell == "O"
       counter += 1
     end
   end
   counter
 end
 
-def current_player(arg)
-  placeholder = turn_count(arg)
-  placeholder % 2 == 0 ? "X" : "O"
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
 end
-# if_this_is_a_true_value ? then_the_result_is_this : else_it_is_this
+
