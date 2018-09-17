2881578a67bb74c188239730ec2e8e7dc5fff8b5
Tam Vu <vutam_a@hotmail.com>
2018-09-17 00:54:44 +0000

    Done.

---
+++ lib/current_player.rb
@@ -0,0 +1,19 @@
+
+def turn_count(board)
+  counter = 0
+  board.each do |turns|
+    if turns == "X" || turns == "O"
+      counter += 1
+    end 
+  end
+  counter
+end
+
+
+def current_player(board)
+  if turn_count(board).to_i % 2 == 0
+    return "X"
+  else
+    return "O"
+  end
+end
