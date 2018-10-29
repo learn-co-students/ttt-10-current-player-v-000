26d8db474158f6ac7c7e6ec188e90d31221a3b54
Thomas Viles <thomas.c.viles.jr@gmail.com>
2018-10-29 16:20:56 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,17 +1,10 @@
 def turn_count(board)
   turns = 0
-  board.each do |position|
-    if position == "X" || position == "O"
-      turns +=1
-    end
-  end
-  return turns
+  board.each {|position| (position == "X" || position == "O") ? turns +=1 : turns}
+  turns
 end
+
 def current_player(board)
   turns = turn_count(board)
-  if turns == 0 || turns % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  (turns == 0 || turns % 2 == 0) ? "X" : "O"
 end
