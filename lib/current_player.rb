c1efbffc8169cebe17c5ece4bc8c9dd96b84bd1f
Vicky  T <vttak9@gmail.com>
2020-01-18 20:05:43 +0000

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -8,12 +8,17 @@ turns_played = 0
 board.each do |spot|
   if spot == "X" || spot == "O"
     turns_played += 1
+
   end
   end
+  return turns_played
 end
 
 def current_player(board)
- board.each do |array| + 1
-   return {array +1}
+ number = turn_count(board)
+ if number % 2 == 0
+  return "X"
+else
+  return "O"
  end
  end
