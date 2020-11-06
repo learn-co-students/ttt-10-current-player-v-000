9069604a7629d5dd65740efffbef357d6380fa94
danpaslawsky <danpaslawsky@gmail.com>
2020-11-06 11:12:17 -0800

    Done.

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,7 @@
+#This file has two methods that finds out whose tuen it is and places an "X"
+#Or an "O" on the board accordingly. 
+
+#accepts an argument of an array and counts how many turns have been taken.
 def turn_count(boardArray)
     counter = 0
     boardArray.each do |turns|
@@ -8,6 +12,8 @@ def turn_count(boardArray)
     counter
 end
 
+#takes in argument of the game board, 
+#calls the turn_count method to determine who's turn it is.
 def current_player(board)
     if turn_count(board) % 2 == 0
         "X"
